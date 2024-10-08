58276a7a22fac7e0228f705a0aec5a788762cb97
damian7197 <damianandresrossi7197@gmail.com>
2024-10-08 20:36:17 -0300

    .

--- login.py
+++ login.py
@@ -1,22 +1,41 @@
 import json
 import os
 
-def crear_usuario():
-    pass
+archivo_usuarios = "usuarios.json"
 
-def iniciar_sesion():
-    usuario = input("Ingrese su nombre de usuario: ").lower
-    print(f"Bienvenido {usuario}!")
-    password = input("Ingrese su contraseña: ")
-    if password == password_correcta:
-        print("Sesion iniciada")
+def cargar_usuarios():
+    if os.path.exists(archivo_usuarios):
+        with open(archivo_usuarios, 'r') as f:
+            return json.load(f)
+    return {}
+
+def guardar_usuarios(usuarios):
+    with open(archivo_usuarios, 'w') as f:
+        json.dump(usuarios, f)
+        
+def crear_usuario(usuarios):
+    nombre_usuario = input("Ingrese su nombre de usuario: ")
+    if nombre_usuario in usuarios:
+        print("El nombre de usuario ya existe, intenta otro")
     else:
-        print("Contraseña incorrecta, intente otra vez")
-        iniciar_sesion()
+        password = input("Introduce tu contraseña: ")
+        usuarios[nombre_usuario] = {'password': password, 'high_score': 0}
+
+def iniciar_sesion(usuarios):
+    nombre_usuario = input("Ingrese su nombre de usuario: ")
+    if nombre_usuario in usuarios:
+        password = input("Ingrese su contraseña: ")
+        if usuarios[nombre_usuario]['password'] == password:
+            print(f"Bienvenido a asteroides {nombre_usuario}")
+        else:
+            print("Contraseña incorrecta.")     
+    else:
+        print("Nombre de usuario incorrecto.")
         
     
 
 def main():
+    usuarios = cargar_usuarios()
     
     while True:
         print("==========Bienvenido a asteroides!===========")
@@ -26,9 +45,9 @@ def main():
         opcion = input("Seleccione la opcion deseada (1,2,3):")
         
         if opcion == 1:
-            crear_usuario()
+            crear_usuario(usuarios)
         elif opcion == 2:
-            iniciar_sesion()
+            iniciar_sesion(usuarios)
         elif opcion == 3:
             print("Gracias por jugar asteroides! Hasta la proxima")
             break
