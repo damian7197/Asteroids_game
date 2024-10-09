6776a2df4b588ad3b22471710b3fa40325174629
damian7197 <damianandresrossi7197@gmail.com>
2024-10-08 21:13:25 -0300

    login

--- login.py
+++ login.py
@@ -1,26 +1,33 @@
 import json
 import os
 
-archivo_usuarios = "usuarios.json"
+archivo_usuarios = "./usuarios.json"
 
+#verifica si el archivo existe, de ser asi lo hace en modo read y lo transforma a un diccionario en python
 def cargar_usuarios():
     if os.path.exists(archivo_usuarios):
         with open(archivo_usuarios, 'r') as f:
             return json.load(f)
     return {}
 
+#toma un diccionario de usuarios y lo convierte a json
 def guardar_usuarios(usuarios):
     with open(archivo_usuarios, 'w') as f:
         json.dump(usuarios, f)
-        
+
+#crea un usuario que va a ser almacenado dentro del archivo        
 def crear_usuario(usuarios):
     nombre_usuario = input("Ingrese su nombre de usuario: ")
     if nombre_usuario in usuarios:
         print("El nombre de usuario ya existe, intenta otro")
     else:
         password = input("Introduce tu contraseña: ")
+        #asignamos caracteristicas al usuario en su respectivo formato
         usuarios[nombre_usuario] = {'password': password, 'high_score': 0}
+        guardar_usuarios(usuarios)
+        print("El usuario ha sido creado con exito!")
 
+#valida el ingreso de usuarios existentes
 def iniciar_sesion(usuarios):
     nombre_usuario = input("Ingrese su nombre de usuario: ")
     if nombre_usuario in usuarios:
@@ -33,7 +40,7 @@ def iniciar_sesion(usuarios):
         print("Nombre de usuario incorrecto.")
         
     
-
+#funcion principal login
 def main():
     usuarios = cargar_usuarios()
     
@@ -44,15 +51,18 @@ def main():
         print("3 - Salir del programa")
         opcion = input("Seleccione la opcion deseada (1,2,3):")
         
-        if opcion == 1:
+        if opcion == "1":
             crear_usuario(usuarios)
-        elif opcion == 2:
+        elif opcion == "2":
             iniciar_sesion(usuarios)
-        elif opcion == 3:
+        elif opcion == "3":
             print("Gracias por jugar asteroides! Hasta la proxima")
             break
         else:
             print("Opcion invalida, intentelo otra vez")
+            
+if __name__=="__main__":
+    main()
                 
 
 
