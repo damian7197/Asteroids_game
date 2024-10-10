451b020b3d0b5199459d90e59f4db62d78cc171e
damian7197 <damianandresrossi7197@gmail.com>
2024-10-10 18:21:11 -0300

    login con re

--- usuarios.py
+++ usuarios.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 
 archivo_usuarios = "./usuarios.json"
 
@@ -14,18 +15,35 @@ def cargar_usuarios():
 def guardar_usuarios(usuarios):
     with open(archivo_usuarios, 'w') as f:
         json.dump(usuarios, f)
+        
+def password_valida(password):
+    return re.match("^[a-zA-Z0-9]{8,}$", password) is not None 
+    
 
 #crea un usuario que va a ser almacenado dentro del archivo        
 def crear_usuario(usuarios):
     nombre_usuario = input("Ingrese su nombre de usuario: ")
     if nombre_usuario in usuarios:
         print("El nombre de usuario ya existe, intenta otro")
-    else:
-        password = input("Introduce tu contraseña: ")
-        #asignamos caracteristicas al usuario en su respectivo formato
-        usuarios[nombre_usuario] = {'password': password, 'high_score': 0}
-        guardar_usuarios(usuarios)
-        print("El usuario ha sido creado con exito!")
+        return
+    
+    
+    while True:
+        password = input("Introduce tu contraseña(alfanumerica y al menos 8 caracteres): ")
+        if not password_valida(password):
+            print("La contraseña no es valida, intentelo otra vez")
+            continue
+        
+        confirm_password = input("Confirme la contraseña ingresada")
+        if password != confirm_password:
+            print("Las contraseñas no coinciden, intentelo de nuevo")
+        else: 
+            #asignamos caracteristicas al usuario en su respectivo formato
+            usuarios[nombre_usuario] = {'password': password, 'high_score': 0}
+            guardar_usuarios(usuarios)
+            print("El usuario ha sido creado con exito!")
+            break
+
 
 #valida el ingreso de usuarios existentes
 def iniciar_sesion(usuarios):
