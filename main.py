fed9db8bea5aec58b47025a4c774aad2f568961a
damian7197 <damianandresrossi7197@gmail.com>
2024-10-15 19:40:53 -0300

    /

--- main.py
+++ main.py
@@ -3,14 +3,23 @@ from constants import *
 
 def main():
     pygame.init()  
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    #definiendo el tamaño de la pantalla
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
+    
+    #creando un objeto clock
+    clock = pygame.time.Clock() 
+    dt = 0 #delta time, por convencion
     
     while True:
-        for event in pygame.event.get():
+        for event in pygame.event.get(): #capturo el evento para cerrar la pantalla
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+        screen.fill((0,0,0)) #los parametros son el color
         pygame.display.flip()
+        #limitar el framerate a 60 FPS
+        dt = clock.tick(60) / 1000
+        
+    
         
 
 if __name__ == "__main__":
