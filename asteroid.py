209bb8f97bd8cc0b0f288469e5e0fee78afe25fb
damian7197 <damianandresrossi7197@gmail.com>
2024-10-19 17:11:32 -0300

    .

--- asteroid.py
+++ asteroid.py
@@ -7,9 +7,11 @@ class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
         
+    #metodo para crear la figura del asteroide
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
         
+    #metodo para actualizar la posicion 
     def update(self, dt):
         self.position += self.velocity * dt
     
@@ -26,6 +28,7 @@ class Asteroid(CircleShape):
         vector_1 = self.velocity.rotate(random_angle)
         vector_2 = self.velocity.rotate(-random_angle)
         
+        #genero los dos nuevos asteroides, ajustando su direccion y velocidad
         new_radius = self.radius - ASTEROID_MIN_RADIUS
         asteroid = Asteroid(self.position.x, self.position.y, new_radius)
         asteroid.velocity = vector_1 * 1.2
